71962a08511bf03491db5b144b4354add1f96af0
Ian <ichen2@lsu.edu>
2020-12-18 21:20:53 +0000

    Modified 'redo' so that 'mainDiv' short code is no longer necessary and page layout can be passed to template directly

--- source/v2/redo.sh
+++ source/v2/redo.sh
@@ -6,15 +6,16 @@ build() {
 
     rm -f $filename.md $filename.html
     cat shortcodes.dhall $filename.dhall | premd-exe > $filename.md
-    pandoc --variable padding=$2 --variable alignment=$3  --template template.htmk -s -o $filename.html $filename.md
+    # each template requires 5 parameters. $1 is the base file, $2-$4 are the padding, $5 is the text alignment
+    pandoc --variable padding="$2 $3 $4" --variable alignment=$5  --template template.htmk -s -o $filename.html $filename.md
     rm -f $filename.md
 }
 
-build landing.dhall "100px 10%" "center"
-build about.dhall "100px 1%" "left"
-build about2.dhall "100px 0 50px 0" "left"
-build team.dhall "100px 0 100px 0" "center"
-build course-descriptions.dhall "100px 4%" "left"
-build course-list.dhall "100px 0" "center"
-build school-list.dhall "100px 10%" "left"
-build pathways.dhall "100px 4%" "left"
+build landing.dhall 100px 10% 100px center
+build about.dhall 100px 1% 100px left
+build about2.dhall 100px 0 50px left
+build team.dhall 100px 0 100px center
+build course-descriptions.dhall 100px 4% 100px left
+build course-list.dhall 100px 0 100 center
+build school-list.dhall 100px 10% 100px left
+build pathways.dhall 100px 4% 100px left
