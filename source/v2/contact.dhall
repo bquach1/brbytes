11d4cadafba27564c46f61be9b882ee84e5a41a5
Ian <ichen2@lsu.edu>
2020-12-21 21:59:57 +0000

    Added contact page

---
+++ source/v2/contact.dhall
@@ -0,0 +1,8 @@
+''
+---
+title: "Contact"
+---
+
+${coloredHeader "indigo" "xxlarge" "Contact Email:"} 
+[info@brbytes.org](mailto:info@brbytes.org)
+''
--- source/v2/redo.sh
+++ source/v2/redo.sh
@@ -20,3 +20,4 @@ build course-list.dhall 100px 0 100px center
 build school-list.dhall 100px 10% 100px left
 build pathways.dhall 100px 4% 100px left
 build coming-soon.dhall 100px 5% 100px center
+build contact.dhall 100px 0 100px center
