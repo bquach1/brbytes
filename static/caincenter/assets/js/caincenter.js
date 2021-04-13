802ab9ac1791cb8058097c19a02ef968d4bd9f00
Fernando Alegre <falegre@brbytes.org>
2021-04-13 16:30:43 -0500

    Ignore case when searching for emails

--- static/caincenter/assets/js/caincenter.js
+++ static/caincenter/assets/js/caincenter.js
@@ -1,41 +1,4 @@
 
-const characters =
-      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-const charactersLength = characters.length;
-
-function anchor(link,desc) { return `<a href="${link}">${desc}</a>`; }
-
-function makeid(length) {
-    let result = '';
-
-    for ( let i = 0; i < length; i++ ) {
-        let idx = Math.floor(Math.random() * charactersLength);
-        result += characters.charAt(idx);
-    }
-
-    return result;
-}
-
-function studentsWait() {
-    let linkon = false;
-    let time = 30;
-    document.getElementById('initial').innerText = time;
-    setInterval(() => {
-        if(time > 0) {
-            document.getElementById('timer').innerText = time;
-            time--; 
-        }
-        else { window.location.replace('students.html'); }
-        if(time < 10 && !linkon) {
-            let fallback = "students-fallback.html?v=" + makeid(7);
-            let newLink = anchor(fallback,"course list.");
-            document.getElementById('courselist').innerHTML = newLink;
-            linkon = true;
-        }
-    }, 1000);
-}
-
 function showCourse(item) {
     let dColor = 'w3-light-green';
     if(item.needs_training === 'yes') dColor = 'w3-orange';
@@ -62,7 +25,7 @@ function mainForm() {
         submitEmail() {
             this.state = 1;
             let url = '/api2/loi21_teacher_submissions'
-                + `?teacher_email=eq.${this.formData.email}`;
+                + `?teacher_email=ilike.${this.formData.email}`;
             fetch(url, {
                 method: "GET",
                 headers: {
